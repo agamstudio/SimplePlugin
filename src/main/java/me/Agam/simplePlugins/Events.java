fdceb57123a9f93477b264f1356a10b982e271f8
Agam <thecrazyagam1@gmail.com>
2025-12-17 15:34:38 +0200

    Initial commit

---
+++ src/main/java/me/Agam/simplePlugins/Events.java
@@ -0,0 +1,36 @@
+package me.Agam.simplePlugins;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+
+public class Events implements Listener {
+
+    @EventHandler
+    public void onShift(PlayerToggleSneakEvent event) {
+        Player player = event.getPlayer();
+
+        if (event.isSneaking()) {
+            Bukkit.broadcastMessage(player.getName() + " עשה פלוץ, תצחקו עליו!!!");
+
+        }
+    }
+
+    @EventHandler
+    public void onServerJoin(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        player.sendMessage("ברוך הבא לשרת!");
+        Bukkit.broadcastMessage("השחקן " + player.getName() + " נכנס לשרת!");
+    }
+
+    @EventHandler
+    public void onServerQuit(PlayerQuitEvent event){
+        Player player = event.getPlayer();
+        Bukkit.broadcastMessage("השחקן " + player.getName() + " יצא מהשרת!");
+    }
+
+}
---
+++ src/main/java/me/Agam/simplePlugins/SimplePlugins.java
@@ -0,0 +1,19 @@
+package me.Agam.simplePlugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimplePlugins extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new Events(), this);
+        System.out.println("הפלאגין הופעל בהצלחה!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("הפלאגין כבוי!");
+    }
+}
