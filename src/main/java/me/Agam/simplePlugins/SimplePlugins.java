97fe0a45f41bf023bee7f478ad26fe4bd26f2768
Agam <thecrazyagam1@gmail.com>
2025-12-22 18:17:03 +0200

    Update SimplePlugins.java

--- src/main/java/me/Agam/simplePlugins/SimplePlugins.java
+++ src/main/java/me/Agam/simplePlugins/SimplePlugins.java
@@ -1,8 +1,26 @@
 package me.Agam.simplePlugins;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class SimplePlugins extends JavaPlugin {
+
+    private File warpsFile;
+    private FileConfiguration warpsConfig;
+
+    public void saveWarps() {
+        try {
+            warpsConfig.save(warpsFile);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public FileConfiguration getWarpsConfig() { return warpsConfig; }
 
     @Override
     public void onEnable() {
@@ -10,8 +28,22 @@ public final class SimplePlugins extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new Events(), this);
         System.out.println("הפלאגין הופעל בהצלחה!");
         this.getCommand("msg").setExecutor(new MsgCommand());
-    }
+        this.getCommand("warp").setExecutor(new CMD_warp());
+        this.getCommand("setwarp").setExecutor(new CMD_setwarp());
 
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
+        }
+
+        warpsFile = new File(getDataFolder(), "warps.yml");
+
+        if (!warpsFile.exists()) {
+            saveResource("warps.yml", false);
+        }
+
+        warpsConfig = YamlConfiguration.loadConfiguration(warpsFile);
+
+    }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
